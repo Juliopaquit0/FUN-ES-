3e750889f3b793ed963c5f4544dd2c1c20d2331e
Juliopaquit0 <julio.alexandre@edu.senai.br>
2026-06-12 09:20:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.AccessControl;
+namespace funcaotabuada;
+
+class ComFuncao
+
+{
+    static void exibirtabuada(int numero)
+    {
+        Console.WriteLine("Qual tabuada deseja imprimir?");
+        numero = int.Parse(Console.ReadLine());
+        int contador = 1;
+
+        while (contador <= 10 )
+        {
+            Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+            contador++;
+        }
+    }
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Qual tabuada deseja imprimir?");
+        int numero = int.Parse(Console.ReadLine());
+        exibirtabuada (numero);
+        
+    }
+
+
+}
+
